4ee59b05c9d4c99e39149f11b3826b8240d7a057
haneeshaachanta54 <haneeshaachanta@gmail.com>
2026-06-21 19:03:22 +0530

    Updated binary to decimal program

--- nested list.py
+++ nested list.py
@@ -1,11 +1,10 @@
-#CONVERT BINARY TO DECIMAL
-n=1011
-d=0
-i=0
-while n!=0:
-    r=n%10
-    d=d+r*pow(2,i)
-    n=n//10
-    i+=1
+# CONVERT BINARY TO DECIMAL
+n = int(input("Enter a binary number: "))
+d = 0
+i = 0
+while n != 0:
+    r = n % 10
+    d = d + r * (2 ** i)
+    n = n // 10
+    i += 1
 print(d)
-
